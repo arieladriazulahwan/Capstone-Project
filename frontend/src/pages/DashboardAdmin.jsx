8a1e307af665d4baad5994570f366b1ea71d8cba
Angga <anggasaputra26112004@gmail.com>
2026-06-08 21:45:57 +0800

    Fix UI Admin: Import missing FiTrendingUp icon causing blank screen

--- frontend/src/pages/DashboardAdmin.jsx
+++ frontend/src/pages/DashboardAdmin.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { FiBookOpen, FiChevronRight, FiFileText, FiHome, FiUsers, FiZap } from "react-icons/fi";
+import { FiBookOpen, FiChevronRight, FiFileText, FiHome, FiUsers, FiZap, FiTrendingUp } from "react-icons/fi";
 import { FaGraduationCap, FaUserCog } from "react-icons/fa";
 
 import Navbar from "../components/Navbar";
